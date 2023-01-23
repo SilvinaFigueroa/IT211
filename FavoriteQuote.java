956b401957c9279ac087e51d6536ffca3b018c1a
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-01-22 17:36:23 -0800

    committing test changes

--- FavoriteQuote.java
+++ FavoriteQuote.java
@@ -7,7 +7,11 @@
  class FavoriteQuote {
     public static void main(String[] args) {
 
+        String git_test = "New Commit in branch, testing";
+        System.out.println(git_test);
+
         String quote = "The limits of my language are the limits of my world";
         System.out.println(quote);
     }
 }   
+ 
