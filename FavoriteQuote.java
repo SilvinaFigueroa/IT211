61abc36961c86c3ad26bc4e9369d4621ec1282cf
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-01-22 17:14:25 -0800

    Learning Activity using git

---
+++ FavoriteQuote.java
@@ -0,0 +1,13 @@
+
+/*
+    [Learning Activity] Can you print your favorite quote?
+
+ */
+
+ class FavoriteQuote {
+    public static void main(String[] args) {
+
+        String quote = "The limits of my language are the limits of my world";
+        System.out.println(quote);
+    }
+}   
---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+/*
+    Class 09/01 - Practise 
+ */
+
+class FirstProgram {
+    public static void main(String[] args) {
+        String debug = "Debuggin my code";
+        System.out.println(debug);
+
+        String quote = "The limits of my language are the limits of my world";
+        System.out.println(quote);
+    }
+}   
