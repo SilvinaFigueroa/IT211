556d2ee1d791dc5c7cfb7cb96bd2b0ce0c22dad4
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-01-22 19:35:34 -0800

    Remove one print line

--- FavoriteQuote.java
+++ FavoriteQuote.java
@@ -3,13 +3,8 @@
     [Learning Activity] Can you print your favorite quote?
 
  */
-
  class FavoriteQuote {
     public static void main(String[] args) {
-
-        String git_test = "New Commit in branch, testing";
-        System.out.println(git_test);
-
         String quote = "The limits of my language are the limits of my world";
         System.out.println(quote);
     }
