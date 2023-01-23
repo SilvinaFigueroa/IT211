9070c8de1faa6baf172b7290d8abb671653eedd4
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-01-22 18:12:44 -0800

    test2

--- FavoriteQuote.java
+++ FavoriteQuote.java
@@ -7,8 +7,8 @@
  class FavoriteQuote {
     public static void main(String[] args) {
 
-        // String git_test = "New Commit in branch, testing";
-        // System.out.println(git_test);
+        String git_test = "New Commit in branch, testing";
+        System.out.println(git_test);
 
         String quote = "The limits of my language are the limits of my world";
         System.out.println(quote);
