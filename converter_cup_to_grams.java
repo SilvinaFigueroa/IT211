dd57e0a6081fcc43237f8b21f1ca518581f55aa4
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-02-05 16:58:02 -0800

    "Assignment" Converter 1: Create your own converter

---
+++ converter_cup_to_grams.java
@@ -0,0 +1,16 @@
+
+
+public class converter_cup_to_grams{
+    public static void main (String [] args){
+        double grams;
+        double cups;
+
+        cups = 2.50;
+
+        grams = cups * 240;
+
+        System.out.println(
+            "The equivalent of " + cups + " cups in grams is " + grams + " grs.");   
+
+    }
+}
