741267b940c95e1923a966600d2be08f1b8b164d
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-02-05 20:16:39 -0800

    Adding nextDouble() to scanner instead of using next and parsing

--- ConverterCupstoGramsUserInput.java
+++ ConverterCupstoGramsUserInput.java
@@ -5,15 +5,11 @@ class ConverterCupsToGrams{
         //Declate variables 
         double numberGrams;
         double  numberCups;
-        String  userInput;
 
         // User input - Cups to be converter into grams 
         Scanner input = new Scanner(System.in);
         System.out.println ("Input the amount of cups to convert into grams");
-        userInput = input.next();
-
-        // Parse the string into double
-        numberCups = Double.parseDouble(userInput);
+        numberCups = input.nextDouble();
 
         //Close Scanner
         input.close();
