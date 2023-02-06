8af320c7d364feb10b7270322e71ba970ddf6353
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-02-05 19:38:46 -0800

    Instructions in the code

--- ConverterCupsToGrams.java
+++ ConverterCupsToGrams.java
@@ -1,13 +1,17 @@
 
 class ConverterCupsToGrams{
     public static void main (String [] args){
+        //Declate variables 
         double numberGrams;
         double numberCups;
 
+        // Number of cups to be converter into grams 
         numberCups = 3.0/4;
 
+        // Equation with the equivalent grams 
         numberGrams = numberCups * 240;
 
+        //Print result 
         System.out.println(
             "The equivalent of " + numberCups + " cups in grams is " + numberGrams + " grs.");   
 
