a01d2115add3bd7b4f0b50be7a6cfb761f68fe7c
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-02-18 20:15:20 -0800

    Secure Password - String Methods

---
+++ StringMethods.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class StringMethods {
+    public static void main(String args[]) {
+
+        // String myString = "";
+  
+        Scanner sc = new Scanner(System.in);
+        String myString = sc.next();
+
+        sc.close();
+
+        // convert String to character array by using toCharArray
+        char[] reverse = myString.toCharArray();
+       
+        String newPass = "";
+
+        // take each character of the string from back to top and add it in a new string
+        for (int i = reverse.length - 1; i >= 0; i--){
+                newPass += (reverse[i]); 
+
+        }
+
+        // Replace vowels for numnbers and special characters
+        newPass = newPass.replace('a', '@');
+        newPass = newPass.replace('e', '3');
+        newPass = newPass.replace('i', '1');
+        newPass = newPass.replace('o', '_');
+        newPass = newPass.replace('u', ')');
+
+        // Convert ever other character in uppercase
+        for (int i = 0; i < newPass.length(); i += 2) {
+            newPass.toUpperCase();
+        }
+
+        System.out.println(newPass);
+    }
+}
