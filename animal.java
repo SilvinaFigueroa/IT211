ba7817457f8cd00a556fde84afef832315bdea4a
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-02-25 16:45:38 -0800

    Fix indentation

--- animal.java
+++ animal.java
@@ -1,10 +1,10 @@
 
-public class animal{
+public class animal {
 
     private String name;
     private int age;
 
-    public animal(String name, int age){
+    public animal(String name, int age) {
         this.age = age;
         this.name = name;
 
@@ -12,42 +12,34 @@ public class animal{
 
 }
 
-    class dog extends animal{
+class dog extends animal {
 
-       public dog(String name, int age) {
-            super(name, age);
-        }
-
-        void bark() {
-            System.out.println("woof woof woof");
-
-        }
-
-
+    public dog(String name, int age) {
+        super(name, age);
     }
 
-    class JindoDog extends dog{
-
-        String oring;
-
-       public  JindoDog(String name, int age, String orign) {
-            super(name, age);
-          
-        }
-
-
-    
-
-    public static void main (String [] args){
-
-        JindoDog myDog = new JindoDog("doggy", 5, "corgui");
-    
-             myDog.bark();
-
+    void bark() {
+        System.out.println("woof woof woof");
 
     }
 
 }
 
+class JindoDog extends dog {
 
+    String oring;
 
+    public JindoDog(String name, int age, String orign) {
+        super(name, age);
+
+    }
+
+    public static void main(String[] args) {
+
+        JindoDog myDog = new JindoDog("doggy", 5, "corgui");
+
+        myDog.bark();
+
+    }
+
+}
