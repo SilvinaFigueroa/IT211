e04a1e4b0cb17fc0486bd078cda433c11492ad58
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-03-12 19:08:04 -0700

    Indentation

--- Text_Game/src/Game.java
+++ Text_Game/src/Game.java
@@ -1,144 +1,139 @@
 import java.util.Scanner;
 
-public class Game{
-    public static void main (String args []){
+public class Game {
+    public static void main(String args[]) {
 
-
-        Scanner userInput = new Scanner (System.in);
+        Scanner userInput = new Scanner(System.in);
 
         System.out.println(
-        "\n _______________________________________________________________" +
-        "\n" +
-        "\n WELCOME TO THE ADVENTURE" +
-        "\n" +
-        "\n _______________________________________________________________");
+                "\n _______________________________________________________________" +
+                        "\n" +
+                        "\n WELCOME TO THE ADVENTURE" +
+                        "\n" +
+                        "\n _______________________________________________________________");
 
         System.out.println("\n Name your characther: \n");
 
-        String name = userInput.nextLine(); 
+        String name = userInput.nextLine();
 
         PlayerClass newPlayer = new PlayerClass(name);
 
         System.out.println("\n Welcome to the game " + name +
-        "\n" +
-        "\n _______________________________________________________________");
+                "\n" +
+                "\n _______________________________________________________________");
 
         System.out.println("\n Chose your race: " +
-        "\n" +
-        "\n Option 1: Elf" +
-        "\n Option 2: Dwarf" +
-        "\n Option 3: Human" +
-        "\n Option 4: Halfling " +
+                "\n" +
+                "\n Option 1: Elf" +
+                "\n Option 2: Dwarf" +
+                "\n Option 3: Human" +
+                "\n Option 4: Halfling " +
 
-        "\n _______________________________________________________________"
-      
+                "\n _______________________________________________________________"
+
         );
 
-        int race = userInput.nextInt(); 
+        int race = userInput.nextInt();
         newPlayer.raceType(race);
 
-          // Select race again 
-          
-          System.out.println("\n Are you happy with your choice? " +
-          "\n" +
-          "\n 1 - YES" +
-          "\n 2 - NO, I want to pick again!" +
-          "\n _______________________________________________________________" 
-          
-          );
+        // Select race again
 
-      int raceChoice = userInput.nextInt(); 
+        System.out.println("\n Are you happy with your choice? " +
+                "\n" +
+                "\n 1 - YES" +
+                "\n 2 - NO, I want to pick again!" +
+                "\n _______________________________________________________________"
 
-         if (raceChoice == 2){
+        );
+
+        int raceChoice = userInput.nextInt();
+
+        if (raceChoice == 2) {
 
             System.out.println("\n Chose your race: " +
-            "\n" +
-            "\n Option 1: Elf" +
-            "\n Option 2: Dwarf" +
-            "\n Option 3: Human" +
-            "\n Option 4: Halfling " +
-    
-            "\n _______________________________________________________________"
-          
+                    "\n" +
+                    "\n Option 1: Elf" +
+                    "\n Option 2: Dwarf" +
+                    "\n Option 3: Human" +
+                    "\n Option 4: Halfling " +
+
+                    "\n _______________________________________________________________"
+
             );
-    
-            int newrace = userInput.nextInt(); 
+
+            int newrace = userInput.nextInt();
             newPlayer.raceType(newrace);
 
-         }
+        }
 
-         else{   System.out.println("Great, let's continue then! " +
-         "\n _______________________________________________________________" 
-         
-         );}
+        else {
+            System.out.println("Great, let's continue then! " +
+                    "\n _______________________________________________________________"
 
+            );
+        }
 
         System.out.println("\n Select a profession for your character: " +
-        "\n" +        
-        "\n Option 1 : Warrior" +          
-        "\n Option 2 : Merchant" +           
-        "\n Option 3 : Assassin" +
-        "\n Option 4 : Magician" +
-        "\n _______________________________________________________________" 
-        );
+                "\n" +
+                "\n Option 1 : Warrior" +
+                "\n Option 2 : Merchant" +
+                "\n Option 3 : Assassin" +
+                "\n Option 4 : Magician" +
+                "\n _______________________________________________________________");
 
-
-        int profession = userInput.nextInt(); 
+        int profession = userInput.nextInt();
         newPlayer.professionType(profession);
 
-           // Select profession again 
+        // Select profession again
         System.out.println("\n Are you happy with your choice? " +
-            "\n" +
-            "\n 1 - YES" +
-            "\n 2 - NO, I want to pick again!" +
-            "\n _______________________________________________________________" 
-            
-            );
+                "\n" +
+                "\n 1 - YES" +
+                "\n 2 - NO, I want to pick again!" +
+                "\n _______________________________________________________________"
 
-        int professionChoice = userInput.nextInt(); 
-
-           if (professionChoice == 2){
-
-            System.out.println("\n Select a profession for your character: " +
-            "\n" +        
-            "\n Option 1 : Warrior" +          
-            "\n Option 2 : Merchant" +           
-            "\n Option 3 : Magician" +
-            "\n Option 4 : Assassin" +
-            "\n _______________________________________________________________" 
-            );
-
-            int newProfession = userInput.nextInt(); 
-            newPlayer.professionType(newProfession);
-
-           }
-
-           else{   System.out.println("Great, let's continue then! " +
-           "\n _______________________________________________________________" 
-           
-           );}
-
-
-        System.out.println("\n Now, you can modify some attributes of your characther: " +
-        "\n" +
-        "\n Option 1 : +3 strength  and - 2 inteligence" +          
-        "\n Option 2 : +2 wisdom 	and - 1 inteligence" +           
-        "\n Option 3 : +3 wisdom    and - 2 strength" +
-        "\n Option 4 : +2 wisdom    and + 1 inteligence but - 4 Strength" +
-        "\n Option 5 : I'm happy with my characther!" +
-
-        "\n _______________________________________________________________"
-      
         );
 
-        int modifier = userInput.nextInt(); 
-        newPlayer.modifiers(modifier);
-        
-        userInput.close();
+        int professionChoice = userInput.nextInt();
 
+        if (professionChoice == 2) {
+
+            System.out.println("\n Select a profession for your character: " +
+                    "\n" +
+                    "\n Option 1 : Warrior" +
+                    "\n Option 2 : Merchant" +
+                    "\n Option 3 : Magician" +
+                    "\n Option 4 : Assassin" +
+                    "\n _______________________________________________________________");
+
+            int newProfession = userInput.nextInt();
+            newPlayer.professionType(newProfession);
+
+        }
+
+        else {
+            System.out.println("Great, let's continue then! " +
+                    "\n _______________________________________________________________"
+
+            );
+        }
+
+        System.out.println("\n Now, you can modify some attributes of your characther: " +
+                "\n" +
+                "\n Option 1 : +3 strength  and - 2 inteligence" +
+                "\n Option 2 : +2 wisdom 	and - 1 inteligence" +
+                "\n Option 3 : +3 wisdom    and - 2 strength" +
+                "\n Option 4 : +2 wisdom    and + 1 inteligence but - 4 Strength" +
+                "\n Option 5 : I'm happy with my characther!" +
+
+                "\n _______________________________________________________________"
+
+        );
+
+        int modifier = userInput.nextInt();
+        newPlayer.modifiers(modifier);
+
+        userInput.close();
 
     }
 
 }
-
-
--- Text_Game/src/PlayerClass.java
+++ Text_Game/src/PlayerClass.java
@@ -1,20 +1,18 @@
 
 public class PlayerClass {
-    private String name; 
+    private String name;
 
-    private String race ="Non defined";
+    private String race = "Non defined";
     private int inteligence = 0;
-    
 
     private String profession;
     private int strength = 0;
     private int wisdom = 0;
-    
-    
-    // Constructor 
+
+    // Constructor
     // Character mandatory attribute: Name
 
-    PlayerClass(String name){
+    PlayerClass(String name) {
         this.name = name;
     }
 
@@ -26,138 +24,138 @@ public class PlayerClass {
         this.name = name;
     }
 
-    public String getRace(){
+    public String getRace() {
         return race;
     }
 
-    public void setRace(String race){
-       this.race = race;
+    public void setRace(String race) {
+        this.race = race;
 
     }
 
-    public void raceType (int optionRace){
-        switch(optionRace){
-            
-            case 1: this.race = "Elf";
-            inteligence = 5;
-            break;
-            
-            case 2: this.race = "Dwarf";
-            inteligence = 4;
-            break;
+    public void raceType(int optionRace) {
+        switch (optionRace) {
 
-            case 3: this.race = "Human";
-            inteligence = 9;
-            break;
+            case 1:
+                this.race = "Elf";
+                inteligence = 5;
+                break;
 
-            case 4: this.race = "Halfling";
-            inteligence = 2;
-            break;
+            case 2:
+                this.race = "Dwarf";
+                inteligence = 4;
+                break;
+
+            case 3:
+                this.race = "Human";
+                inteligence = 9;
+                break;
+
+            case 4:
+                this.race = "Halfling";
+                inteligence = 2;
+                break;
         }
 
         System.out.println("\n Great, this is how your character looks like so far: " +
-        "\n" +
-        "\n Name: " + this.name +
-        "\n     Race: " + this.race +
-        "\n             Inteligence: " + this.inteligence +
+                "\n" +
+                "\n Name: " + this.name +
+                "\n     Race: " + this.race +
+                "\n             Inteligence: " + this.inteligence +
 
-        "\n _______________________________________________________________"
-        
+                "\n _______________________________________________________________"
+
         );
 
+    }
 
-     } 
+    public void professionType(int optionProfession) {
+        switch (optionProfession) {
 
-        
-     public void professionType (int optionProfession){
-        switch(optionProfession){
-            
-            case 1: this.profession = "Warrior";
-            strength = 8;
-            wisdom = 2;
-            break;
+            case 1:
+                this.profession = "Warrior";
+                strength = 8;
+                wisdom = 2;
+                break;
 
-            case 2: this.profession = "Merchant";
-            strength = 5;
-            wisdom = 5;
-            break;
+            case 2:
+                this.profession = "Merchant";
+                strength = 5;
+                wisdom = 5;
+                break;
 
-            case 3: this.profession = "Assassin";
-            strength = 4;
-            wisdom = 6;
-            break;
+            case 3:
+                this.profession = "Assassin";
+                strength = 4;
+                wisdom = 6;
+                break;
 
-            case 4: this.profession = "Magician";
-            strength = 7;
-            wisdom = 4;
-            break;
+            case 4:
+                this.profession = "Magician";
+                strength = 7;
+                wisdom = 4;
+                break;
         }
 
-
         System.out.println("\n Now we have completed your character: " +
-        "\n" +
-        "\n Name: " + this.name +
-        "\n     Race: " + this.race +
-        "\n             Inteligence: " + this.inteligence +
-        "\n             Profession: " + this.profession +
-        "\n             Strength: " + this.strength +
-        "\n             Wisdom: " + this.wisdom +
+                "\n" +
+                "\n Name: " + this.name +
+                "\n     Race: " + this.race +
+                "\n             Inteligence: " + this.inteligence +
+                "\n             Profession: " + this.profession +
+                "\n             Strength: " + this.strength +
+                "\n             Wisdom: " + this.wisdom +
 
-        "\n _______________________________________________________________"
-        
+                "\n _______________________________________________________________"
+
         );
 
+    }
 
+    public void modifiers(int modifier) {
 
-     }
-
-     public void modifiers(int modifier){
-
-        switch(modifier){  
+        switch (modifier) {
 
             case 1: // +3 strength and - 2 inteligence
-            strength = this.strength + 3;
-            inteligence = this.inteligence - 2;
-            break;
+                strength = this.strength + 3;
+                inteligence = this.inteligence - 2;
+                break;
 
             case 2: // +2 wisdom and - 1 inteligence
-            wisdom = this.wisdom + 2;
-            inteligence = this.inteligence - 1;
-            break;
+                wisdom = this.wisdom + 2;
+                inteligence = this.inteligence - 1;
+                break;
 
             case 3: // +3 wisdom and - 2 strength
-            wisdom = this.wisdom + 3;
-            strength = this.strength - 2;
-            break;
+                wisdom = this.wisdom + 3;
+                strength = this.strength - 2;
+                break;
 
             case 4: // +2 wisdom and + 1 inteligence but - 4 Strength
-            strength = this.strength - 4;
-            inteligence = this.inteligence + 1;
-            wisdom = this.wisdom + 2;
-            break;
+                strength = this.strength - 4;
+                inteligence = this.inteligence + 1;
+                wisdom = this.wisdom + 2;
+                break;
 
-            case 5: 
-            break;
+            case 5:
+                break;
         }
 
         System.out.println("\n Ok, those are your character features: " +
-        "\n" +
-        "\n Name: " + this.name +
-        "\n     Race: " + this.race +
-        "\n             Inteligence: " + this.inteligence +
-        "\n             Profession: " + this.profession +
-        "\n             Strength: " + this.strength +
-        "\n             Wisdom: " + this.wisdom +
+                "\n" +
+                "\n Name: " + this.name +
+                "\n     Race: " + this.race +
+                "\n             Inteligence: " + this.inteligence +
+                "\n             Profession: " + this.profession +
+                "\n             Strength: " + this.strength +
+                "\n             Wisdom: " + this.wisdom +
 
-        "\n _______________________________________________________________"
+                "\n _______________________________________________________________"
 
         );
 
         System.out.println("\n Let the game begin!");
         System.out.println("\n END");
-     }
-
-
     }
-    
 
+}
