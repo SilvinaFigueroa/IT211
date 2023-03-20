94069f92c377e7c98791ae10bc0bc5f3d65d3c2c
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-03-19 23:02:21 -0700

    Creating Board1 and Board2

--- TicTacToe/src/TicTacToeDemo.java
+++ TicTacToe/src/TicTacToeDemo.java
@@ -4,21 +4,39 @@
 class TicTacToeDemo {
     public static void main(String[] args) {
 
-        TicTacToe board = new TicTacToe();
+        TicTacToe board1 = new TicTacToe();
         
-        board.printBoard();
-        board.updateBoard(1, 1);
-        board.updateBoard(2, 0);
-        board.updateBoard(1, 0);
-        board.updateBoard(2, 1);
-        board.updateBoard(0, 2);
-        board.updateBoard(0, 0);
-        board.updateBoard(0, 1);
-        board.updateBoard(1, 2);
-        board.updateBoard(2, 2);
-        
+        // Test a TIE
+
+        System.out.println("\n------------------\n");
+        System.out.println("Game Board1 starts");
+        System.out.println("------------------\n");
+
+        board1.printBoard();
+        board1.updateBoard(1, 1);
+        board1.updateBoard(2, 0);
+        board1.updateBoard(1, 0);
+        board1.updateBoard(2, 1);
+        board1.updateBoard(0, 2);
+        board1.updateBoard(0, 0);
+        board1.updateBoard(0, 1);
+        board1.updateBoard(1, 2);
+        board1.updateBoard(2, 2);
+
+        // Test a WINNER match
+        TicTacToe board2 = new TicTacToe();
+
+        System.out.println("\n------------------");
+        System.out.println("Game Board2 starts");
+        System.out.println("------------------\n");
 
 
+        board2.printBoard();
+        board2.updateBoard(1, 1);
+        board2.updateBoard(2, 0);
+        board2.updateBoard(1, 0);
+        board2.updateBoard(2, 1);
+        board2.updateBoard(1, 2);
 
 
     }
